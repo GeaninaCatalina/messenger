ff8bc5a1e453676b4da8d4f3035d66156e7e88df
Geanina <masgras.geanina@hotmail.com>
2020-03-16 21:21:32 +0100

    react messenger app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Grid, Card } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Wizzard</h1>
+        <Grid columns={3}>
+          <Grid.Row>
+            <Grid.Column>
+              <Card>User1</Card>
+            </Grid.Column>
+            <Grid.Column>
+              <h1>Here are your message</h1>
+            </Grid.Column>
+            <Grid.Column>
+              <Card>User1</Card>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default App;
