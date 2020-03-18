62220e4494e4001dd5c6c7a26228271341338bc2
Geanina Catalina Masgras <masgras.geanina@hotmail.com>
2020-03-18 18:31:52 +0100

    Geanina: Added background and changed h1 style

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ class App extends Component {
         <h1>Wizzard</h1>
         <Grid columns={3} divided>
           <Grid.Row>
-            <Grid.Column>
+            <Grid.Column centered={true}>
             <Card> <Image src='https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_1280.png' wrapped ui={false} />
               <Card.Content>
                 <Card.Header>User</Card.Header>
               </Card.Content>
             </Card>
             </Grid.Column>
-            <Grid.Column>
-              <h1>Here are your message</h1>
+            <Grid.Column centered={true}>
+              <h2>Here are your messages</h2>
             </Grid.Column>
-            <Grid.Column>
+            <Grid.Column Align='right' >
             <Card> <Image src='https://cdn.pixabay.com/photo/2015/11/03/10/23/watercolor-1020509_1280.jpg' wrapped ui={false} />
               <Card.Content>
                 <Card.Header>User2</Card.Header>
