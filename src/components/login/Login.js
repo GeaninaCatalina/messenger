e99315a8c7668391aab0efc3068dde2d70f73e3a
Geanina Catalina Masgras <masgras.geanina@hotmail.com>
2020-04-08 22:30:21 +0200

    Geanina: Add login form

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import './Login.css';
-import {Link} from "react-router-dom";
-import {Button} from 'semantic-ui-react';
+import { Link } from "react-router-dom";
+import { Button, Form } from 'semantic-ui-react';
 
 class Login extends Component {
-    render() {
-      return (
-        <div className='AppHeder'>
+  render() {
+    return (
+      <div>
+        <Form size='big' className='openPage'>
+          <Form.Input fluid label='Name' placeholder='Name' width='10'/>
+          <Form.Input fluid label='Password' placeholder='Password' width='10'/>
           <Button className='openButton' color='yellow' size='big'>
-            <Link to='/messenger'>Go to messenger</Link>
-          </Button>
-        </div>
-      )
-    }
+          <Link to='/messenger'>Go to messenger</Link>
+        </Button>
+        </Form>
+   </div>
+    )
   }
-  
+}
+
 export default Login;
