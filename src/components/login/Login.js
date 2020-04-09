96679c66e7ac1ec87ee8661b7242610549ba7d8d
Geanina Catalina Masgras <masgras.geanina@hotmail.com>
2020-04-09 20:41:34 +0200

    Geanina: Draft - make log in functional

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,8 +2,54 @@ import React, { Component } from 'react';
 import './Login.css';
 import { Link } from "react-router-dom";
 import { Button, Form } from 'semantic-ui-react';
+import axios from 'axios';
 
 class Login extends Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    user: "",
+    password: "",
+    loginErrors: ""
+  };
+
+  this.handleSubmit = this.handleSubmit.bind(this);
+  this.handleChange = this.handleChange.bind(this);
+}
+
+handleChange(event) {
+  this.setState({
+    [event.target.name]: event.target.value
+  });
+}
+
+handleSubmit(event) {
+  const { name, password } = this.state;
+
+  axios
+    .post(
+      "http://localhost:3000/",
+      {
+        user: {
+          name: name,
+          password: password
+        }
+      },
+      { withCredentials: true }
+    )
+    .then(response => {
+      if (response.data.logged_in) {
+        this.props.handleSuccessfulAuth(response.data);
+      }
+    })
+    .catch(error => {
+      console.log("login error", error);
+    });
+  event.preventDefault();
+}
+
   render() {
     return (
       <div>
